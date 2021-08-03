4ad39f9fe04d69ac7a20a6ddd5635bc8ffdd1e20
vitor <vitorbarbosa789@gmail.com>
2021-08-02 22:44:38 -0300

    Aprovação correção

--- Semana1/aprovacao/aprovacao.py
+++ Semana1/aprovacao/aprovacao.py
@@ -5,7 +5,7 @@
 # Objetivo do código: Retornar ao usuário sua próxima unidade e a sua média. 
 
 unidade = int(input('Unidade? '))
-media = float(input())
+media = float(input('Média de aprovação na unidade? '))
 proxima_unidade = unidade + 1
 
 print(f'\nO aluno vai para a unidade {proxima_unidade} com média {media:.1f}.')
