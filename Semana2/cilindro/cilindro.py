aca265ffc9cf8dce7d936c3bd9d96db40a7e1d51
vitor <vitorbarbosa789@gmail.com>
2021-10-13 11:22:06 -0300

    Correções exercício semana 2

--- Semana2/cilindro/cilindro.py
+++ Semana2/cilindro/cilindro.py
@@ -4,20 +4,19 @@
 # Atividade-titulo: Área do cilindro 
 # Objetivo do código: 
 
-diametro = float(input('Medida do diâmetro '))
-altura = float(input('Medida da altura? '))
+diametro = float(input())
+altura = float(input())
 
 raio = diametro / 2 
 area_base = 3.141592653589793 * ( raio ** 2 )
 
 perimetro_base = (3.141592653589793 * 2) * raio
 area_lateral_cilindro = altura * perimetro_base 
-
 area_cilindro = (area_base * 2) + area_lateral_cilindro
 
 print("Cálculo da Superfície de um Cilindro")
 print("---")
-print(f'Medida do diâmetro? {diametro:.2f}')
-print(f'Medida da altura? {altura:.2f}')
+print(f'Medida do diâmetro? {diametro}')
+print(f'Medida da altura? {altura}')
 print("---")
 print(f'Área calculada: {area_cilindro:.2f}')
